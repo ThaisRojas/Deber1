4b20eaef073c89e7ece635ec7fccab258e1034e3
Thais Rojas <thais.rojas@udla.edu.ec>
2023-10-16 20:29:57 -0500

    fin

--- ejercicio42.c
+++ ejercicio42.c
@@ -13,11 +13,25 @@ int Maximo(int matriz[3][4]) {
     }
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 4; j++) {
-            if (matriz[i][j] == maximo) {
+            if (matriz[i][j] == max) {
                 contador++;
             }
         }
     }
 
     return contador;
+}
+
+int main() {
+    int matriz[3][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12}
+    };
+
+    int repeticiones = Maximo(matriz);
+
+    printf("El valor máximo se repite %d veces en la matriz.\n", repeticiones);
+
+    return 0;
 }
