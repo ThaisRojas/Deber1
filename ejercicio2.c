8ed6ef336537600219c5472beea484b7c4bf9ad2
Thais Rojas <thais.rojas@udla.edu.ec>
2023-10-16 22:37:19 -0500

    usuario

--- ejercicio2.c
+++ ejercicio2.c
@@ -3,3 +3,10 @@
 int UltimosDigitos(int numero) {
     return numero % 100; 
 }
+
+int main() {
+    int numero, resultado;
+
+    printf("Ingrese un número entero: ");
+    scanf("%d", &numero);
+}
