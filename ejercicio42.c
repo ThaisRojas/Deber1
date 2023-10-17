7706c39249740e16346ee5ab529058efbb66c701
Thais Rojas <thais.rojas@udla.edu.ec>
2023-10-16 20:26:33 -0500

    Encontrar el valor maximo

---
+++ ejercicio42.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int Maximo(int matriz[3][4]) {
+    int max = matriz[0][0];
+    int contador = 0;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (matriz[i][j] > max) {
+                max = matriz[i][j];
+            }
+        }
+    }
+}
