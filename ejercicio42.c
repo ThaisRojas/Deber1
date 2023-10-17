a605d2e2f7f91fcfe5d44f750e118aa958c74ee5
Thais Rojas <thais.rojas@udla.edu.ec>
2023-10-16 20:28:23 -0500

    Cuenta las veces de repeticion

--- ejercicio42.c
+++ ejercicio42.c
@@ -11,4 +11,13 @@ int Maximo(int matriz[3][4]) {
             }
         }
     }
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (matriz[i][j] == maximo) {
+                contador++;
+            }
+        }
+    }
+
+    return contador;
 }
