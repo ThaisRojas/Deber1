fd1c267c0bc56c53dcf86c738f11a14349be7aa2
Thais Rojas <thais.rojas@udla.edu.ec>
2023-10-16 22:22:35 -0500

    fin

--- ejercicio22.c
+++ ejercicio22.c
@@ -12,4 +12,12 @@ int main() {
 
     printf("Ingrese un número entero: ");
     scanf("%d", &valor);
+
+    if (esMultiploDe5(valor)) {
+        printf("%d es múltiplo de 5.\n", valor);
+    } else {
+        printf("%d no es múltiplo de 5.\n", valor);
+    }
+
+    return 0;
 }
